dcb2e90bcd22e3582719e5a42ba534647ebc446f
Alexandru <alexandru.mucenic@kubisinteractive.com>
2024-07-03 15:19:47 +0300

    gsap horizontal scroll done, json data wip, styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".hero__panel"); // converting the items into an array
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1), //
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".hero",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1), //auto slide after a little scoll
+    end: () => "+=" + document.querySelector(".hero").offsetWidth,
+  },
+});
